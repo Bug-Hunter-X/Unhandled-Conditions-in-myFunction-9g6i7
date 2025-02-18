24837d0e731d29cae8353389c2581d898cd969a6
BugHunter-X <askzenai@gmail.com>
2025-02-18 14:52:04 +0100

    Add bug.m file

---
+++ bug.m
@@ -0,0 +1,11 @@
+function result = myFunction(input)
+  % Some code here...
+  if someCondition
+    result = someValue;
+  else
+    result = anotherValue;
+  end
+  % More code here...
+end
+
+% This function may not handle all cases properly. For example, if someCondition is always false, or there are other unhandled conditions. This could lead to unexpected results or errors.
